a6a45930c3a93e3e1552b38eb5efde8acbdb62c8
new26463 <new26463@gmail.com>
2020-02-17 15:06:06 +0700

    add employee template

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee.component.scss']
 })
 export class EmployeeComponent implements OnInit {
-
+  employeeCol: string[] = ['name', 'department', 'position', 'edit', 'profile', 'delete'];
+  employee = [{
+    'name': 'satoshi',
+    'department': 'programmer',
+    'position': 'employee',
+  },{
+    'name': 'masayoshi',
+    'department': 'manager',
+    'position': 'ceo',
+  },{
+    'name': 'new',
+    'department': 'hr',
+    'position': 'employee',
+  },{
+    'name': 'amp',
+    'department': 'graphic',
+    'position': 'employee',
+  },
+];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  createEmployee() {
+
+  }
+
+  delete() {
+    console.log('delete');
   }
 
 }
